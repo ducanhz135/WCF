b2ec3d182ccf369ab25c7cdb5f5249b10b4d7d04
duc anh <ducanhz135@gmail.com>
2020-05-03 23:44:05 +0700

    commit

---
+++ DemoWCFAndLinq/WcfServiceDemo/IServiceCateogory.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace WcfServiceDemo
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IServiceCateogory" in both code and config file together.
+    [ServiceContract]
+    public interface IServiceCateogory
+    {
+        [OperationContract]
+        void DoWork();
+
+        [OperationContract]
+        List<User> FindAll();
+
+        [OperationContract]
+        User Find(int userCd);
+
+        [OperationContract]
+        bool Insert(string userName);
+
+        [OperationContract]
+        bool Update(int userCd);
+
+        [OperationContract]
+        bool Delete(int userCd);
+    }
+}
---
+++ DemoWCFAndLinq/WcfServiceDemo/ServiceCateogory.svc.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace WcfServiceDemo
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "ServiceCateogory" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select ServiceCateogory.svc or ServiceCateogory.svc.cs at the Solution Explorer and start debugging.
+    public class ServiceCateogory : IServiceCateogory
+    {
+        public bool Delete(int userCd)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DoWork()
+        {
+        }
+
+        public User Find(int userCd)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<User> FindAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Insert(string userName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Update(int userCd)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
